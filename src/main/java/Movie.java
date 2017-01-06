719287eb056da2cd1c494c57168ac692a0470e94
skoryupina <e.skoryupina.develop@ya.ru>
2017-01-07 00:25:54 +0400

    Replace Conditional with Polymorphism

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -4,52 +4,42 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode){
         _title= title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
+    }
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
-    public void setPriceCode(int _priceCode) {
-        this._priceCode = _priceCode;
-    }
 
     public String getTitle() {
         return _title;
     }
 
     double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == NEW_RELEASE)
-                &&
-                daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
---
+++ src/main/java/NewReleasePrice.java
@@ -0,0 +1,15 @@
+public class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
+}
